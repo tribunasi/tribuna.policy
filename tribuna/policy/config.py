58dc411a61a6d9a9edda17b2486d9c8670decf02
Termitnjak d.o.o <info@termitnjak.si>
2013-11-20 18:16:02 +0000

    add setuphandler which creates initial folder structure and tags user. Also added registry.xml with discussion and diazo settings

---
+++ tribuna/policy/config.py
@@ -0,0 +1,18 @@
+INITIAL_STRUCTURE = [
+    {
+        'title': 'Tags folder',
+        'type': 'Folder'
+    },
+    {
+        'title': 'Articles folder',
+        'type': 'Folder'
+    },
+    {
+        'title': 'Images folder',
+        'type': 'Folder'
+    },
+    {
+        'title': 'Entry pages',
+        'type': 'Folder'
+    },
+]
--- tribuna/policy/setuphandlers.py
+++ tribuna/policy/setuphandlers.py
@@ -1,30 +1,70 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from plone import api
+from tribuna.policy.config import INITIAL_STRUCTURE
 
-def runCustomCode(site):
-    """ Run custom add-on product installation code to modify Plone site object and others
 
-    @param site: Plone site
+def create_tags_user(portal):
+    """Create tags user, which is needed for working with tags.
+
+    XXX: Make it work without this!
+
+    :param site: Plone site
     """
 
-    api.user.create(username="tags_user", roles=("Member", ), email="tags_user2@tags.com")
-    api.user.grant_roles(username="tags_user", roles=["Site Administrator", "Member", "Manager", "Editor", "Reader", "Contributor", "Reviewer"])
+    api.user.create(
+        username="tags_user", roles=("Member", ), email="info@termitnjak.si")
+    api.user.grant_roles(
+        username="tags_user",
+        roles=[
+            "Site Administrator",
+            "Member",
+            "Manager",
+            "Editor",
+            "Reader",
+            "Contributor",
+            "Reviewer",
+        ]
+    )
 
 
+def create_structure(portal):
+    """Create folder structure."""
 
-def setupVarious(context):
-    """
-    @param context: Products.GenericSetup.context.DirectoryImportContext instance
-    """
+    # delete default Plone content
+    for item_id in ('Members', 'news', 'events', 'front-page'):
+        api.content.delete(portal[item_id])
 
+    # create root folders
+    for item in INITIAL_STRUCTURE:
+        obj = api.content.create(
+            container=portal,
+            type=item['type'],
+            title=item['title'],
+        )
+        api.content.transition(obj, transition='publish')
+
+    # create an entry page and set it as default view
+    entry_page = api.content.create(
+        container=portal['entry-pages'],
+        type='tribuna.content.entrypage',
+        title='Hello',
+        text=u'Hello'
+    )
+    api.content.transition(entry_page, transition='publish')
+    portal['entry-pages'].default_page = entry_page.id
+
+    # set default view also on root
+    portal.default_page = 'entry-pages'
+
+
+def setup_various(context):
     # We check from our GenericSetup context whether we are running
     # add-on installation for your product or any other proudct
-    if context.readDataFile('your.package.marker.txt') is None:
+    if context.readDataFile('tribuna.policy.setup_various.txt') is None:
         # Not your add-on
         return
 
-    portal = context.getSite()
-
-    runCustomCode(portal)
+    portal = api.portal.get()
+    create_tags_user(portal)
+    create_structure(portal)
