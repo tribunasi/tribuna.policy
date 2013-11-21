b537b50dd6b3acb76e419a9d96b1ecacf4825c3a
Termitnjak d.o.o <info@termitnjak.si>
2013-11-21 11:58:14 +0000

    add sidebar portlet installation step

--- tribuna/policy/setuphandlers.py
+++ tribuna/policy/setuphandlers.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from plone import api
+from plone.portlets.constants import CONTEXT_CATEGORY
+from plone.portlets.interfaces import ILocalPortletAssignmentManager
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletAssignmentMapping
+from tribuna.content.portlets.sidebar import Assignment
 from tribuna.policy.config import INITIAL_STRUCTURE
+from zope.component import getUtility
+from zope.component import getMultiAdapter
+from zope.container.interfaces import INameChooser
 
 
 def create_tags_user(portal):
@@ -58,6 +66,29 @@ def create_structure(portal):
     portal.default_page = 'entry-pages'
 
 
+def add_sidebar_portlet(portal):
+    """Add sidebar portlet (with tags, filters etc.) and block all other
+    portlets.
+    """
+    # Block parent portlets
+    for col in (u'plone.rightcolumn', u'plone.leftcolumn', ):
+        portlet_manager = getUtility(
+            IPortletManager, name=col, context=portal)
+        assignable = getMultiAdapter(
+            (portal, portlet_manager,), ILocalPortletAssignmentManager)
+        assignable.setBlacklistStatus(CONTEXT_CATEGORY, True)
+
+    # Create a sidebar portlet assignment
+    mapping = getMultiAdapter(
+        (portal, portlet_manager,),
+        IPortletAssignmentMapping,
+        context=portal
+    )
+    assignment = Assignment()
+    chooser = INameChooser(mapping)
+    mapping[chooser.chooseName(None, assignment)] = assignment
+
+
 def setup_various(context):
     # We check from our GenericSetup context whether we are running
     # add-on installation for your product or any other proudct
@@ -68,3 +99,4 @@ def setup_various(context):
     portal = api.portal.get()
     create_tags_user(portal)
     create_structure(portal)
+    add_sidebar_portlet(portal)
